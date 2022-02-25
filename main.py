d848494e58e6cc8ff71853593c55a11e7ca766fb
abhishekghz <sunny7739094749@gmail.com>
2022-02-25 13:51:44 +0530

    Complete web_scraping project

---
+++ main.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+search = input("Search for: ")
+params = {"q": search}
+r = requests.get("https://www.bing.com/search",params=params)
+
+soup = BeautifulSoup(r.text, "html.parser")
+results = soup.find("ol", {"id": "b_results"})
+links = results.findAll("li", {"class": "b_algo"})
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
+
+        children = item.find("h2")
+        print("Next Sibling of the h2: ",children.next_sibling)
